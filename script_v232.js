077c8ab3700577771453e1984e22d26cac9f2680
marshall-shendu-ma <marshma@bu.edu>
2025-08-19 21:21:50 -0400

    V3.26

--- script_v232.js
+++ script_v232.js
@@ -92,8 +92,8 @@ document.addEventListener('DOMContentLoaded', function() {
     colors = labels.map(l => l.includes('High')? 'rgb(230,75,53)' : l.includes('Intermediate')? 'rgb(254,168,9)' : 'rgb(3,160,135)');
     res.innerHTML = '<ul style="margin-left:20px;">' + labels.map((l,i)=>`<li><strong>${l}:</strong> ${data[i].toFixed(1)}%</li>`).join('') + '</ul>';
     if(modelChart) modelChart.destroy();
-    modelChart=new Chart(document.getElementById('modelChart'), {  type:'bar',  data:{labels,datasets:[    {label:'% Risk',data,backgroundColor:colors},    {type:'line',label:'80% threshold',data:labels.map(()=>80),     borderColor:'red',borderWidth:2,borderDash:[6,6],pointRadius:0,fill:false}  ]},
-      options:{ plugins:{annotation:{annotations:{line1:{type:'line',yMin:80,yMax:80,borderColor:'red',borderWidth:2,borderDash:[6,6]}}}}, 
+    modelChart=new Chart(document.getElementById('modelChart'), {  type:'bar',  data:{labels,datasets:[    {label:'% Risk',data,backgroundColor:colors},    {type:'line',label:'80% threshold',data:labels.map(()=>80),borderColor:'red',borderWidth:5,borderDash:[6,6],pointRadius:0,fill:false}  ]},
+      options:{ plugins:{annotation:{annotations:{line1:{type:'line',yMin:80,yMax:80,borderColor:'red',borderWidth:5,borderDash:[6,6]}}}}, 
         scales:{
           x:{ grid:{lineWidth:5}, ticks:{font:{size:20}} },
           y:{ beginAtZero:true, max:100, grid:{lineWidth:5}, ticks:{font:{size:20}}}
